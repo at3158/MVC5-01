335eac88138323ae1772df8a5d366125f143ebed
jain <at3158@hotmail.com>
2015-12-31 17:55:12 +0800

    add controller+view
    add controller+view

---
+++ MVC5-01/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5_01.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name,int ID=1)
+        {
+            return HttpUtility.HtmlEncode("Welcome "+name+ ", ID is " + ID);
+        }
+    }
+}
